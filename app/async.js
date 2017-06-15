2879df5ffd583ffd8c57c07f104088904a541876
Vitaliy Subbotskyy <cent.post@live.com>
2017-06-15 16:36:52 +0300

    promises and async

--- app/async.js
+++ app/async.js
@@ -2,10 +2,14 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    return Promise.resolve(value);
   },
 
   manipulateRemoteData: function(url) {
-
+    return new Promise((resolve, reject) => {
+        $.ajax(url).then(function(data){
+            resolve(data.people.map((person) => { return person.name }).sort());
+        });
+    });
   }
 };
