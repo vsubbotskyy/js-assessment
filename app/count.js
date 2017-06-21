b388c0ffd378501bc0834fb57b4695da662b26e8
Vitaliy Subbotskyy <cent.post@live.com>
2017-06-21 13:47:21 +0300

    setInterval

--- app/count.js
+++ app/count.js
@@ -1,7 +1,23 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
+  number: 0,
   count: function (start, end) {
+    number = start;
+    console.log(number);
+    var countInterval = setInterval(function(){
+        if (number < end) {
+            console.log(++number);
+        }
+        else {
+            clearInterval(countInterval);
+        }
+    }, 100);
 
+    var cancelTimer = function cancelTimer() {
+        clearInterval(countInterval);
+    }
+
+    return { cancel: cancelTimer };
   }
 };
