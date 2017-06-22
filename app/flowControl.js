eec458758ccb64fb4b6c65bceff023d9b9da9767
Vitaliy Subbotskyy <cent.post@live.com>
2017-06-22 13:19:22 +0300

    flow control

--- app/flowControl.js
+++ app/flowControl.js
@@ -10,6 +10,19 @@ exports.flowControlAnswers = {
     //
     // otherwise the function should return the number, or false if no number
     // was provided or the value provided is not a number
+    if (!num || isNaN(num)) {
+        return false;
+    }
+    if (num % 3 === 0) {
+        if (num % 5 === 0) {
+            return 'fizzbuzz';
+        }
+        return 'fizz';
+    }
+    if (num % 5 === 0) {
+        return 'buzz';
+    }
 
+    return num;
   }
 };
